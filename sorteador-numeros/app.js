8ed18637529f356d834ac68f1852a70445dc473c
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-09 13:25:36 -0300

    Sorteador de Numeros 0.2 (Agora ele sorteia e mostra a lista no console do navegador)

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,13 +1,30 @@
 //Variaveis
 
-let quantidadeDeNumeros;
-let numeroInicial;
-let numeroFinal;
+let numerosSorteados = [];
+
 
 //Funções
 
-function Aleatorizar(params) {
-    
+function sortear() {
+    let numerosSorteados = [];
+    let quantidade = parseInt(document.getElementById('quantidade').value);
+    let min = parseInt(document.getElementById('de').value);
+    let max = parseInt(document.getElementById('ate').value);
+   
+    if (quantidade > (max - min + 1)) {
+        alert("Erro: A quantidade de números solicitada é maior do que o intervalo disponível.");
+        return;
+    };
+   
+    while (quantidade > 0) {
+        let numeroAleatorio = Math.floor(Math.random() * (max - min + 1)) + min;
+        if (numerosSorteados.includes(numeroAleatorio)) {
+            continue;
+        };
+        numerosSorteados.push(numeroAleatorio);
+        quantidade--;
+    };
+    console.log(numerosSorteados);
 }
 
 //TextoHTML
