25362a3683a4d74af6c582e0d367ad513ac4794a
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-10 11:12:20 -0300

    Sorteador de numeros 1.0
    Agora ele funciona como deveria.

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,7 +1,10 @@
+//Iniciar
+
 //Variaveis
 
 let numerosSorteados = [];
-
+let textoSingular = 'O numero sorteado foi: ';
+let textoPlural = 'Os numeros sorteados foram: ';
 
 //Funções
 
@@ -25,6 +28,43 @@ function sortear() {
         quantidade--;
     };
     console.log(numerosSorteados);
+    botaoAtivo();
+    if (numerosSorteados.length == 1) {
+        exibirTexto('.texto__paragrafo__final',`${textoSingular}${numerosSorteados}`);
+    } else {
+        exibirTexto('.texto__paragrafo__final',`${textoPlural}${numerosSorteados}`);
+
+    };
+};
+
+function reiniciar() {
+    botaoDesativo();
+    limparCampo();
+    exibirTexto('.texto__paragrafo__final', `Os numeros ainda nao foram sorteados.`);
 }
 
+function limparCampo() {
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+}
+
+function botaoAtivo() {
+    let botao = document.getElementById('btn-reiniciar');
+    botao.classList.remove('container__botao-desabilitado');
+    botao.classList.add('container__botao');
+};
+
+function botaoDesativo() {
+    let botao = document.getElementById('btn-reiniciar');
+    botao.classList.remove('container__botao');
+    botao.classList.add('container__botao-desabilitado');
+};
+
+function exibirTexto(seletor, novoTexto) {
+    const elemento = document.querySelector(seletor);
+    elemento.innerHTML = novoTexto;
+};
+
 //TextoHTML
+exibirTexto('.texto__paragrafo__final', `Os numeros ainda nao foram sorteados.`);
