b38173cce6b3df37f8cbd5bb1aacb265a435d18b
Gabriel Ramos <sanchesramosg@gmail.com>
2024-12-16 11:59:10 -0300

    Sorteio nomes 0.1
    A função de sortear eu peguei desse cara (https://pt.stackoverflow.com/users/5878/woss)
    Creditos todos para ele.
    Estou com fome e sem cabeça para seguir programando agora entao parei por ai mesmo

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,19 @@
+//--Funções--
+
+//--Créditos da função para (https://pt.stackoverflow.com/users/5878/woss) que usou
+//--o super cérebro dele para pensar algo que não fui capaz.
+function pares(nomes) {
+    const _pairs = [];
+    const _nomes = [...nomes];
+    _nomes.sort(() => Math.random() - 0.5);
+    for (let i = 0; i < _nomes.length; i++) {
+        _pairs.push([_nomes[i], _nomes[(i != _nomes.length - 1) ? i + 1 : 0]]);
+    }
+    return _pairs;
+};
+
+const nomes = [ ];
+
+function sortear() {
+    console.log(pares(nomes));
+}
